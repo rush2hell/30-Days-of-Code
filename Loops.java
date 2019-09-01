6e87868483fb5e8601e9f5125f1f11e9347fb04d
rush2hell <47297909+rush2hell@users.noreply.github.com>
2019-09-01 12:31:53 +0530

    Add files via upload

---
+++ Loops.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Loops
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int result=0;
+        for(int i=1;i<11;i++)
+        {
+            result = n*i;
+            System.out.println(n+" x "+i+" = "+result);
+        }
+    }
+}
